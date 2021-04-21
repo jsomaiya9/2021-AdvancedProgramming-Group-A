5c51527cd1a56356a19bf3860b598c74c0614c8e
Marius Cautis <912703@my.leicestercollege.ac.uk>
2021-04-21 12:48:10 +0100

    2

--- FootbalManager/TheFootballFanClub/Global.asax.cs
+++ FootbalManager/TheFootballFanClub/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using TheFootballFanClub.Models;
 
 namespace TheFootballFanClub
 {
@@ -12,6 +14,7 @@ namespace TheFootballFanClub
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DataInitialiser());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ FootbalManager/TheFootballFanClub/Models/DataInitialiser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheFootballFanClub.Models
+{
+    public class DataInitialiser: DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+        }
+}
